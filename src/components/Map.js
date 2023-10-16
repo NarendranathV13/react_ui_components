2537fa08b3bacdb22d68f3763e7dbe1e3e5c7865
NarendranathV13 <narendranath.v@arkinfotec.com>
2023-10-16 19:26:35 +0530

    updated

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,12 +10,12 @@ const Map = () => {
   const userLocation = { // Default location
     lat: 40.7128,
     lng: -74.0060
-  };
+  };// coimbatore
   return (
     <LoadScript googleMapsApiKey="AIzaSyDZmKnQpPM0beDs8Zd_pMyM0PIwGxgPD4s">
       <GoogleMap
         mapContainerStyle={containerStyle}
-        center={userLocation} // Use user's location if available, otherwise use default
+        center={userLocation} // location to be displayed
         zoom={10}
       >
         {userLocation && <Marker position={userLocation} />}
